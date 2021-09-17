b510fdf8c644e0cfdc159e19fbd4944f0cf4c07f
Olena-web <73849375+Olena-web@users.noreply.github.com>
2021-09-18 00:44:22 +0300

    fix: remove line with error

--- src/count-cats.js
+++ src/count-cats.js
@@ -15,7 +15,7 @@ import { NotImplementedError } from '../extensions/index.js';
  *
  */
 export default function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
+  
   // remove line with error and write your code here
   const countCats = (arr) => {
   let cat = "^^";
