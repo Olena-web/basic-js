a94ace9aab07523bc289dd4171f4c2a07521b75e
Olena Honcharuk <gasmin07@gmail.com>
2021-09-18 23:50:08 +0300

    fix according to test

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,24 +1,23 @@
-import { NotImplementedError } from '../extensions/index.js';
+import { NotImplementedError } from "../extensions/index.js";
 
 /**
  * Create name of dream team based on the names of its members
- *  
- * @param {Array} members names of the members 
+ *
+ * @param {Array} members names of the members
  * @return {String | Boolean} name of the team or false
  * in case of incorrect members
  *
  * @example
- * 
+ *
  * createDreamTeam(['Matt', 'Ann', 'Dmitry', 'Max']) => 'ADMM'
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-export default function createDreamTeam(/* members */) {
-  
-  
+export default function createDreamTeam(members) {
+  if (!Array.isArray(members)) return false;
   let secretObj = members.map((i) => {
     if (typeof i === "string") {
-      return i[0];
+      return i.trimLeft()[0].toUpperCase();
     }
   });
   const nameTeam = secretObj.sort().join("");
@@ -28,5 +27,3 @@ export default function createDreamTeam(/* members */) {
     return false;
   }
 }
-
-
