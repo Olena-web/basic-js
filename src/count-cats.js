d4c3421e4ad397a7bdc6e62f41b79d6ed6308c11
Olena-web <73849375+Olena-web@users.noreply.github.com>
2021-09-18 23:21:54 +0300

    fix: remove extra line

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,14 +14,10 @@ import { NotImplementedError } from '../extensions/index.js';
  * ]) => 3`
  *
  */
-export default function countCats(/* matrix */) {
-  
-  // remove line with error and write your code here
-  const countCats = (arr) => {
+export default function countCats(arr) {
   let cat = "^^";
   let string = [].concat(...arr).toString();
-
   return string.split(",").reduce((acc, ch) => (ch === cat ? acc + 1 : acc), 0);
 };
 
-}
+
