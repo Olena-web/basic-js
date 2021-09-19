21356d329f731fb5631dbf0575c17bc5c89cb448
Olena Honcharuk <gasmin07@gmail.com>
2021-09-19 14:51:38 +0300

    fix: getSeason

--- src/what-season.js
+++ src/what-season.js
@@ -14,25 +14,20 @@ import { NotImplementedError } from "../extensions/index.js";
 export default function getSeason(date) {
   if (!date) {
     return "Unable to determine the time of year!";
-  } else if (
-    Object.prototype.toString.call(date) !== "[object Date]" ||
-    !Object.keys(date).length
-  ) {
+  }
+  try {
+    date.toUTCString();
+  } catch (error) {
     throw new Error("Invalid date!");
+  }
+  let month = date.getMonth();
+  if (month == 11 || month == 0 || month == 1) {
+    return "winter";
+  } else if (month == 2 || month == 3 || month == 4) {
+    return "spring";
+  } else if (month == 5 || month == 6 || month == 7) {
+    return "summer";
   } else {
-    let month = date.getMonth();
-    if (month == 12 || month == 1 || month == 2) {
-      return "winter";
-    } else if (month == 3 || month == 4 || month == 5) {
-      return "spring";
-    } else if (month == 6 || month == 7 || month == 8) {
-      return "summer";
-    } else {
-      return "autumn";
-    }
+    return "autumn";
   }
 }
-console.log(getSeason(new Date(1154, 1, 22, 23, 45, 11, 500)));
-console.log(getSeason(new Date(1900, 4, 17, 11, 27, 4, 321)));
-console.log(getSeason(new Date(22, 6, 11, 23, 45, 11, 500)));
-console.log(getSeason(new Date(1, 8, 26, 3, 0, 11, 500)));
